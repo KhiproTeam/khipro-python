e7ba3eaa72bc0fdb9a51f41fab5e52b738580d4a
Nafee <abdunnafialif@gmail.com>
2026-04-23 09:49:19 +0600

    INIT_state এর NIL_Match অংশ ফিক্স করা

--- khipro.py
+++ khipro.py
@@ -60,8 +60,7 @@ def tokenizer(string:str, list_of_dictionaries:list[dict]):
                 break  # অর্থাৎ, ম্যাচ পাওয়া গেছে। স্লাইস যত বড়োই হোক, ব্রেক করতে হবে।
             elif decision == 0:
                 pointer -= 1  # যদি ম্যাচ না পাওয়া যায় তাহলে পয়েন্টার একঘর বামে সরাবে। তাহলে প্রসেসেবল স্ট্রিং ছোটো হবে। লুপ আবার চলবে।
-            
-
+           
         return (decision, pointer)
 
 def init_state(string:str):
@@ -73,7 +72,7 @@ def init_state(string:str):
     if decision == 0:  # যদি এখনও no-match অবস্থা থাকে তাহলে... ## NIL_MATCH
         output_stream.append(string[:pointer]) # অর্থাৎ প্রথম ক্য‍ারেক্টার, যেটা ম্যাচ হয়নি, সেটাকেই ইংরেজি অবস্থাতেই পাঠিয়ে দেওয়া হচ্ছে।
         init_state(refundable_slice(string, pointer))
-        return (output, refundable_slice(string, pointer))
+        
 
     elif decision not in {0, -1}: # যদি এক বা একাধিক ম্যাচ পাওয়া যায়...
         if 0 in decision: # shor
